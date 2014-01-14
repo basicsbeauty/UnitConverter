eb41f2d06d116a8caa2ac79b4cd9a6e9e97e5501
BVSS <bvs.siva@yahoo.com>
2014-01-13 21:18:11 -0800

    Initial Commit

---
+++ TempConverter/TCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TCAppDelegate.h
+//  TempConverter
+//
+//  Created by Beauty on 1/13/14.
+//  Copyright (c) 2014 Ranam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TempConverter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TempConverter
+//
+//  Created by Beauty on 1/13/14.
+//  Copyright (c) 2014 Ranam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TCAppDelegate class]));
+    }
+}
